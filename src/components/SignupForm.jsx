e93fd7e472e47585f65079dc26219d8c0058f0f9
Alex Nurci <alexnurci@gmail.com>
2025-07-07 14:48:13 -0400

    signunform and logout button

---
+++ src/components/SignupForm.jsx
@@ -0,0 +1,55 @@
+import { useState } from "react";
+
+function SignupForm() {
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [message, setMessage] = useState("");
+
+    const handleSignup = async (e) => {
+        e.preventDefault();
+
+        try{
+            const res = await fetch("/auth/signup", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                credentials: "include",
+                body: JSON.stringify({ username, password })
+            });
+            const data = await res.json();
+
+            if(!res.ok){
+                throw new Error(data.error || "Singup failed");
+            }
+
+            setMessage("Signup succseful!");
+        } catch (error) {
+            setMessage(error.message);
+        }
+    };
+
+    return (
+        <form onSubmit={handleSignup}>
+            <h2>Sign up</h2>
+            <input
+                type="text"
+                value={username}
+                placeholder="User"
+                onChange={(e) => setUsername(e.target.value)}
+                required
+            />
+            <input
+                type="text"
+                value={password}
+                placeholder="Password"
+                onChange={(e) => setPassword(e.target.value)}
+                required
+            />
+            <button type="submit">Sign Up</button>
+            <p>{message}</p>
+        </form>
+    );
+}
+
+export default SignupForm;
